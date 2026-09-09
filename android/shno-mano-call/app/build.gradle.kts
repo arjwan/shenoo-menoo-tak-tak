d74f4987429c03a1de0946a6f376da77cf4c0ee4
arjwan <arjwan307@gmail.com>
2026-09-09 20:44:07 +0300

    Accept Iraqi birth date format during signup

--- android/shno-mano-call/app/build.gradle.kts
+++ android/shno-mano-call/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.shnomano.call"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.4"
+        versionCode = 8
+        versionName = "1.2.5"
     }
 
     signingConfigs {
