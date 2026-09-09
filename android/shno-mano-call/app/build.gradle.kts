460290434cc034f04065347ace8662dea6e8c19f
arjwan <arjwan307@gmail.com>
2026-09-09 22:36:26 +0300

    Add QR scanner dependency for friend adding

--- android/shno-mano-call/app/build.gradle.kts
+++ android/shno-mano-call/app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
     implementation("androidx.room:room-runtime:2.6.1")
@@ -91,4 +91,5 @@ dependencies {
     implementation("io.socket:socket.io-client:2.1.1") {
         exclude(group = "org.json", module = "json")
     }
+    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
