df5c3bb48897b51c78008149c58727a529d43147
arjwan <arjwan307@gmail.com>
2026-09-09 22:36:46 +0300

    Keep lifecycle dependency version while adding QR support

--- android/shno-mano-call/app/build.gradle.kts
+++ android/shno-mano-call/app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
     implementation("androidx.room:room-runtime:2.6.1")
