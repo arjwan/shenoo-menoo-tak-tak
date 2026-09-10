edcfce5aa918220d7f39c928a485d58cd15913f5
arjwan <arjwan307@gmail.com>
2026-09-11 01:15:53 +0300

    Load homepage notification dropdown assets

--- auth-guard.js
+++ auth-guard.js
@@ -24,6 +24,20 @@
     document.head.appendChild(profilePosts);
   }
 
+  if (/\/(?:taktak\.html)?$/i.test(location.pathname)) {
+    if (!document.querySelector('link[data-home-notifications]')) {
+      var notificationsCss = document.createElement('link');
+      notificationsCss.rel = 'stylesheet';
+      notificationsCss.href = 'home-notifications.css?v=20260911-1';
+      notificationsCss.setAttribute('data-home-notifications', '1');
+      document.head.appendChild(notificationsCss);
+    }
+    var notificationsScript = document.createElement('script');
+    notificationsScript.src = 'home-notifications.js?v=20260911-1';
+    notificationsScript.defer = true;
+    document.head.appendChild(notificationsScript);
+  }
+
   var token = localStorage.getItem('token') || sessionStorage.getItem('token');
   if (!token) window.location.replace('signin.html');
 })();
