f0166e96c62cdf827f6e40312851b158ba908dd3
arjwan <arjwan307@gmail.com>
2026-09-10 23:04:28 +0300

    Load profile post timeline on profile page

--- auth-guard.js
+++ auth-guard.js
@@ -12,6 +12,13 @@
     document.head.appendChild(fixes);
   }
 
+  if (/\bprofile\.html$/i.test(location.pathname)) {
+    var profilePosts = document.createElement('script');
+    profilePosts.src = 'profile-posts.js?v=20260910-1';
+    profilePosts.defer = true;
+    document.head.appendChild(profilePosts);
+  }
+
   var token = localStorage.getItem('token') || sessionStorage.getItem('token');
   if (!token) window.location.replace('signin.html');
 })();
